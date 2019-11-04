7e74640540d76ffb76f9361f6c138762bdc324f5
Wen Yang Ho <howenyang@gmail.com>
2019-11-04 00:49:03 -0500

    lumen_setup.sh to the correct path

--- lumen_setup.sh
+++ lumen_setup.sh
@@ -1,5 +1,7 @@
 docker exec -it app composer create-project laravel/lumen .
+cd html
 mv app/public/* app/public/.* app
 mv app/public/public/* app/public/public/.* app/public
 rmdir app/public/public
+cd ..
 
